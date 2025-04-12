fe4db34ed58542c398ef095e468ec9997f243f81
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-04-13 00:28:02 +0200

    Permiresimi i kodit per enkripimin permes Algorimit Myszkowski

--- src/main/java/MyszkowskiTransposition.java
+++ src/main/java/MyszkowskiTransposition.java
@@ -1,30 +1,51 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class MyszkowskiTransposition {
     public static void main(String[] args) {
-
+        String message = "MUHAMED JAKUPI";
+        String key = "HELLO";
+        String ciphertext = Myszkowski.encrypt(message, key);
+        System.out.println("Original: " + message);
+        System.out.println("Ciphertext: " + ciphertext);
     }
+}
+
+class Myszkowski {
     public static String encrypt(String message, String key) {
-        while(key.length()<message.length()) {
-            key+=key;
-        }
+        message = message.replaceAll(" ", "").toUpperCase();
+        key = key.toUpperCase();
 
-        char[] messageArray=message.toCharArray();
-        char[] keyArray=key.toCharArray();
-        int[] sortedKeyIndexes=new int[key.length()];
+        int cols = key.length();
+        int rows = (int) Math.ceil((double) message.length() / cols);
 
-        for(int i=0; i<key.length(); i++) {
-            sortedKeyIndexes[i]=i;
-        }
-
-        Arrays.sort(sortedKeyIndexes);
-
-        StringBuilder encryptedMessage=new StringBuilder();
-        for(int i=0; i<key.length(); i++) {
-            for(int j=1; j<message.length(); j+=key.length()) {
-                encryptedMessage.append(messageArray[j]);
+        char[][] matrix = new char[rows][cols];
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = (index < message.length()) ?
+                        message.charAt(index++)
+                        : ' ';
             }
         }
-        return encryptedMessage.toString();
+
+        TreeMap<Character, List<Integer>> keyMap = new TreeMap<>();
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            if (!keyMap.containsKey(c)) {
+                keyMap.put(c, new ArrayList<>());
+            }
+            keyMap.get(c).add(i);
+        }
+
+        StringBuilder cipherText = new StringBuilder();
+        for (List<Integer> columns : keyMap.values()) {
+            for (int row = 0; row < rows; row++) {
+                for (int col : columns) {
+                    cipherText.append(matrix[row][col]);
+                }
+            }
+        }
+
+        return cipherText.toString().replaceAll(" ", "");
     }
 }
